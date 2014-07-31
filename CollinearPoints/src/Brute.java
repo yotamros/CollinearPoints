870006f1c44d1ccb1a30f805a848965d4aa29435
Yotam Rosenbaum <yotamrosen@gmail.com>
2014-07-31 13:57:46 -0500

    Change points.

--- CollinearPoints/src/Brute.java
+++ CollinearPoints/src/Brute.java
@@ -2,10 +2,11 @@
 public class Brute {
 
     public static void main(String[] args) {
+        
         Point point1 = new Point(1, 2);
-        Point point2 = new Point(1, 2);
-        Point point3 = new Point(1, 2);
-        Point point4 = new Point(1, 2);
+        Point point2 = new Point(3, 4);
+        Point point3 = new Point(5, 2);
+        Point point4 = new Point(4, 10);
 
     }
 
